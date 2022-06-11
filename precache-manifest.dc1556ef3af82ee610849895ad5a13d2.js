d33d5debc236de534946abb35f0b9b10ed4aa928
Abuzar Khan <105778612+abuzarkhan1@users.noreply.github.com>
2022-06-11 12:26:47 +0500

    Updates

---
+++ precache-manifest.dc1556ef3af82ee610849895ad5a13d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a17b2d9f3e7c6c7acf3",
+    "url": "/React-ToDo/static/css/main.5e83541e.chunk.css"
+  },
+  {
+    "revision": "3a17b2d9f3e7c6c7acf3",
+    "url": "/React-ToDo/static/js/main.3a17b2d9.chunk.js"
+  },
+  {
+    "revision": "67c260d117dd46f276ce",
+    "url": "/React-ToDo/static/js/1.67c260d1.chunk.js"
+  },
+  {
+    "revision": "dab80533524147f66ed6",
+    "url": "/React-ToDo/static/js/runtime~main.dab80533.js"
+  },
+  {
+    "revision": "81aa8124767e50f1090e1b6515809b6d",
+    "url": "/React-ToDo/index.html"
+  }
+];
